dd0cf6069096894614441efe9d8f4fcffbe2824d
amirhnajafiz <najafizadeh21@gmail.com>
2025-10-20 10:58:14 -0400

    feat: e2e test

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -5,6 +5,9 @@ set -e
 # get the script directory
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+echo "Running end-to-end test for file-access-patterns..."
+echo "Logs will be stored in $SCRIPT_DIR/logs, and will be removed after the test."
+
 # rm the old logs directory if it exists
 rm -rf "$SCRIPT_DIR/logs"
 # create a new logs directory
@@ -21,7 +24,10 @@ docker run -it \
   -v /usr/src:/usr/src:ro \
   -v "$SCRIPT_DIR/logs":/logs:rw \
    file-access-patterns:test \
-  /usr/local/app/trace.sh -c "ls" -o logs/trace_ls.txt
+  /usr/local/app/trace.sh -c "ls" -o /logs/trace_ls.txt
 
 # display the generated log file
 cat "$SCRIPT_DIR/logs/trace_ls.txt"
+
+# remove the logs directory after the test
+rm -rf "$SCRIPT_DIR/logs"
