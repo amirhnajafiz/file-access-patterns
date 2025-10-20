222bcfd71975000131cc7895308d91dafa66c550
amirhnajafiz <najafizadeh21@gmail.com>
2025-10-20 10:53:36 -0400

    add: persistant logs of e2e test

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# rm the old logs directory if it exists
+rm -rf logs
+# create a new logs directory
+mkdir -p logs
+
 docker run -it \
   --privileged \
   --user root \
@@ -13,4 +18,7 @@ docker run -it \
   -v /usr/src:/usr/src:ro \
   -v "$(pwd)/logs":/logs:rw \
    file-access-patterns:test \
-  /usr/local/app/trace.sh -c "ls" -o logs
+  /usr/local/app/trace.sh -c "ls" -o logs/trace_ls.txt
+
+# display the generated log file
+cat logs/trace_ls.txt
