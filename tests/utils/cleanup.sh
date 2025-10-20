272e422ba24da692617f23bd1182400ea211efa3
amirhnajafiz <najafizadeh21@gmail.com>
2025-10-20 11:03:04 -0400

    fix: cleanup script bug

--- tests/utils/cleanup.sh
+++ tests/utils/cleanup.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-docker rm -f file-access-patterns:test || true
+# remove any existing docker container with the image name file-access-patterns:test
+docker rm -f $(docker ps -a --filter "ancestor=file-access-patterns:test" --format "{{.ID}}") || true
+
+# remove the docker image file-access-patterns:test
+docker rmi -f file-access-patterns:test || true
