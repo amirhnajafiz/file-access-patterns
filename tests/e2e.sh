c25da595724df06ef37cc5f72187f1d34f7187b7
amirhnajafiz <najafizadeh21@gmail.com>
2025-10-20 10:51:25 -0400

    add: e2e test

---
+++ tests/e2e.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+docker run -it \
+  --privileged \
+  --user root \
+  --net=host \
+  --pid=host \
+  --ulimit memlock=-1 \
+  -v /lib/modules:/lib/modules:ro \
+  -v /sys/kernel/debug:/sys/kernel/debug:rw \
+  -v /usr/src:/usr/src:ro \
+   file-access-patterns:test \
+  /usr/local/app/trace.sh -c "ls" -o tmp
