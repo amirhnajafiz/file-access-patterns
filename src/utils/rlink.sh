c86144a36af763a1da8d60f079497845fca508ac
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-05 13:55:01 -0500

    add: rlink in utils

---
+++ src/utils/rlink.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# file: rlink.sh
+
+# Usage:
+#   ./rlink.sh <PID> <FD>
+
+set -eu
+
+# set the pid and file descriptor
+pid=$1
+fd=$2
+
+# return the absolute path
+sudo readlink -f "/proc/${pid}/fd/${fd}"
