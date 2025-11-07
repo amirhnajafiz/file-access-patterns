cc6831e28aff8e4fa1246f6d4bbacb6eba51b8c3
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-07 13:17:17 -0500

    fix: test suits

--- src/tests/test_cgroups.sh
+++ src/tests/test_cgroups.sh
@@ -6,28 +6,16 @@ set -eu
 directory="bpftrace/cgroups"
 
 # test `tracings` scripts
-sudo bpftrace -dd "${directory}/cgroup_trace.bt" 1
+sudo bpftrace -dd "${directory}/cgroup_trace.bt" 1 0
 exit_status=$?
 if [ "$exit_status" -ne 0 ]; then
     echo "cgroup_trace.bt failed"
 fi
 
-sudo bpftrace -dd -c "ls" "${directory}/cgroup_comm_trace.bt" 1 tmp
+sudo bpftrace -dd -c "ls" "${directory}/cgroup_comm_trace.bt" 1 tmp 1
 exit_status=$?
 if [ "$exit_status" -ne 0 ]; then
     echo "cgroup_comm_trace.bt failed"
-fi
-
-sudo bpftrace -dd "${directory}/minimal/cgroup_trace.bt" 1
-exit_status=$?
-if [ "$exit_status" -ne 0 ]; then
-    echo "minimal/cgroup_trace.bt failed"
-fi
-
-sudo bpftrace -dd -c "ls" "${directory}/minimal/cgroup_comm_trace.bt" 1 tmp
-exit_status=$?
-if [ "$exit_status" -ne 0 ]; then
-    echo "minimal/cgroup_comm_trace.bt failed"
 fi
 
 echo "passed all tests."
