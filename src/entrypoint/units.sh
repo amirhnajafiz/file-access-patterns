16d31ca6649da7d4517cccbd21d709f4cf9cf6cd
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-11 16:37:12 -0500

    fix: utils bug

--- src/entrypoint/units.sh
+++ src/entrypoint/units.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # file: entrypoint/units.sh
 
-sudo ./tests/test_cgroups.sh
-sudo ./tests/test_tracings.sh
-sudo ./tests/test_filter.sh
+./tests/test_cgroups.sh
+./tests/test_tracings.sh
+./tests/test_filter.sh
