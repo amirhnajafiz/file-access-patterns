d16e8f4d7dfb40b80075e01112b60505256fb4c6
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-18 10:24:05 -0500

    remove: filter

--- lttng/start.sh
+++ lttng/start.sh
@@ -17,12 +17,10 @@ lttng enable-channel --session="${SESSION}" --kernel --subbuf-size=30M chan2
 # enable system-call events
 lttng enable-event --session="${SESSION}" --kernel --syscall --channel=chan1 \
     read,write,pread64,pwrite64,readv,writev,preadv,pwritev,mmap \
-    --filter '$ctx.cgroup_ns == "${CGROUP}"'
 
 # enable kprobes events
 lttng enable-event --session="${SESSION}" --kernel --channel=chan2 \
     block_rq_complete,block_rq_issue,mm_page_free,mm_page_alloc,kmem_mm_page_alloc,kmem_mm_page_free \
-    --filter '$ctx.cgroup_ns == "${CGROUP}"'
 
 # enable context options
 lttng add-context --session="${SESSION}" --kernel \
