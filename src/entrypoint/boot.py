c8c5dc265c7b8d673437ea3415f162b89fc9b4ff
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-24 13:53:05 -0500

    fix: env set issue

--- src/entrypoint/boot.py
+++ src/entrypoint/boot.py
@@ -37,7 +37,7 @@ signal.signal(signal.SIGTERM, handle_shutdown)
 
 def main():
     global GOUTPUT_PATH
-    os.environ['BPFTRACE_MAX_STRLEN'] = 1024
+    os.environ['BPFTRACE_MAX_STRLEN'] = str(1024)
 
     parser = argparse.ArgumentParser(
         description="Bootstraps a tracing session for a pod/container. "
