5bb5701e2099c19c05cc1593d08d1c356a18015e
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-25 14:19:52 -0500

    update: j2 template generator

--- scripts/gen_bpftrace.py
+++ scripts/gen_bpftrace.py
@@ -1,14 +1,17 @@
 # file: scripts/gen_bpftrace.py
-
-# generating bpftrace scripts
+# generating bpftrace scripts by reading the j2 files in `templates` directory.
 
 from jinja2 import Template
 import json
+import os
 
 
 
-def import_enteries(path: str) -> list:
-    """import the scripts data to build scripts based on templates.
+CONFIG_PATH = "templates/tracers.json"
+
+
+def import_json(path: str) -> dict:
+    """import json data into a dictionary.
     """
     data = {}
     with open(path, "r") as file:
@@ -39,23 +42,37 @@ def save_template(path: str, out: str) -> None:
 
 
 if __name__ == "__main__":
-    # load scripts
-    enteries = import_enteries("templates/bpftraces.json")
+    # load the configs
+    cfg = import_json(CONFIG_PATH)
 
-    # build scripts based on templates
-    for entry in enteries:
-        tmp = read_template(entry["source"])
-        begin_section = read_to_str(entry["begin"])
-        filter_section = read_to_str(entry["filter"])
+    # form the template paths
+    templates_dir_path = os.path.join(cfg["templates_dir"], cfg["sources_dir"])
 
-        out = tmp.render(
-            begin_section=begin_section, 
-            filter=filter_section,
-            file_name=entry["file_name"],
-            usage_cmd=entry["usage_cmd"],
-            enable_subprocess=entry["enable_subprocess"],
-            subprocess=read_to_str(entry["subprocess"]),
-            enable_kfuncs=entry["enable_kfuncs"]
-        )
+    # generate bpftrace scripts by going through inputs and sources
+    for entry in cfg["inputs"]:
+        # form the paths
+        dir_path = os.path.join(cfg["templates_dir"], cfg["inputs_dir"], entry)
+        filter_path = os.path.join(dir_path, "filter.bt")
+        begin_path = os.path.join(dir_path, "begin.bt")
+        output_dir_path = os.path.join(cfg["outputs_dir"], entry)
+
+        os.makedirs(output_dir_path, exist_ok=True)
+
+        # read inputs
+        filter_section = read_to_str(filter_path)
+        begin_section = read_to_str(begin_path)
+
+        # create the outputs
+        for out in cfg["sources"]:
+            # form the paths
+            template_path = os.path.join(templates_dir_path, out) + ".j2"
+            output_path = os.path.join(output_dir_path, out)
+
+            tmp = read_template(template_path)
+
+            res = tmp.render(
+                begin_section=begin_section,
+                filter=filter_section
+            )
         
-        save_template(entry["out"], out)
+            save_template(output_path , res)
