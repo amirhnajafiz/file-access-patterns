7188f9d813a69f593653b9d4ec9fde395e3d86d6
amirhnajafiz <najafizadeh21@gmail.com>
2025-11-26 10:28:27 -0500

    refactor: v1

--- core/entrypoint/v1/src/handlers.py
+++ core/entrypoint/v1/src/handlers.py
@@ -6,6 +6,14 @@ from utils import ensure_script
 
 
 def handle_execute(output_dir: str, execute: str) -> list[Tracer]:
+    """Handle the execute command.
+
+    running: bpftrace -o output -c "command" bpftrace/execute/<tracer>.bt
+
+    :param output_dir: tracing output directory
+    :param execute: the command to execute
+    :return: list of tracing scripts
+    """
     tracers = []
 
     for tname, tpath in get_tracing_scripts("bpftrace/execute").items():
@@ -19,7 +27,16 @@ def handle_execute(output_dir: str, execute: str) -> list[Tracer]:
 
     return tracers
 
+
 def handle_pid(output_dir: str, pid: str) -> list[Tracer]:
+    """Handle the pid tracing.
+
+    running: bpftrace -o output bpftrace/pid/<tracer>.bt <pid>
+
+    :param output_dir: tracing output directory
+    :param pid: the pid to trace
+    :return: list of tracing scripts
+    """
     tracers = []
 
     for tname, tpath in get_tracing_scripts("bpftrace/pid").items():
@@ -33,7 +50,16 @@ def handle_pid(output_dir: str, pid: str) -> list[Tracer]:
 
     return tracers
 
+
 def handle_command(output_dir: str, command: str) -> list[Tracer]:
+    """Handle the command tracing.
+
+    running: bpftrace -o output bpftrace/command/<tracer>.bt <command>
+
+    :param output_dir: tracing output directory
+    :param command: the command to trace
+    :return: list of tracing scripts
+    """
     tracers = []
 
     for tname, tpath in get_tracing_scripts("bpftrace/command").items():
@@ -47,7 +73,19 @@ def handle_command(output_dir: str, command: str) -> list[Tracer]:
 
     return tracers
 
-def handle_cgroup_and_command(output_dir: str, cgid: str, filter_command: str) -> list[Tracer]:
+
+def handle_cgroup_and_command(
+    output_dir: str, cgid: str, filter_command: str
+) -> list[Tracer]:
+    """Handle the cgroup and command tracing.
+
+    running: bpftrace -o output bpftrace/cgroup_and_command/<tracer>.bt <cgroup> <command>
+
+    :param output_dir: tracing output directory
+    :param cgid: the cgroup to trace
+    :param filter_command: the command to filter
+    :return: list of tracing scripts
+    """
     tracers = []
 
     for tname, tpath in get_tracing_scripts("bpftrace/cgroup_and_command").items():
@@ -61,7 +99,16 @@ def handle_cgroup_and_command(output_dir: str, cgid: str, filter_command: str) -
 
     return tracers
 
+
 def handle_cgroup(output_dir: str, cgid: str) -> list[Tracer]:
+    """Handle the cgroup tracing.
+
+    running: bpftrace -o output bpftrace/cgroup/<tracer>.bt <cgroup>
+
+    :param output_dir: tracing output directory
+    :param cgid: the cgroup to trace
+    :return: list of tracing scripts
+    """
     tracers = []
 
     for tname, tpath in get_tracing_scripts("bpftrace/cgroup").items():
