48f9af3b661f87fa57b1cbcccd25080e276c6f2b
amirhnajafiz <najafizadeh21@gmail.com>
2025-12-23 13:20:32 -0500

    update: config env bind

--- operator/include/configs/configs.go
+++ operator/include/configs/configs.go
@@ -1,6 +1,8 @@
 package configs
 
 import (
+	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -9,25 +11,55 @@ import (
 // Config hold the operator tune parameters.
 // all configuration parameters must be set as environment variables or as a `.env` file.
 type Config struct {
-	LogLevel string `mapstructure:"FLAP_LOG_LEVEL"`
-	JSONLog  bool   `mapstructure:"FLAP_JSON_LOG"`
-	TLS      bool   `mapstructure:"FLAP_TLS"`
+	LogLevel string `mapstructure:"flap.log_level" env:"FLAP_LOG_LEVEL"`
+	JSONLog  bool   `mapstructure:"flap.json_log"  env:"FLAP_JSON_LOG"`
+	TLS      bool   `mapstructure:"flap.tls"       env:"FLAP_TLS"`
+}
+
+func bindEnvs(v *viper.Viper, iface interface{}) error {
+	val := reflect.ValueOf(iface)
+	if val.Kind() != reflect.Ptr {
+		return fmt.Errorf("bindEnvs expects a pointer")
+	}
+
+	val = val.Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := typ.Field(i)
+
+		key := field.Tag.Get("mapstructure")
+		env := field.Tag.Get("env")
+
+		if key == "" || env == "" {
+			continue
+		}
+
+		if err := v.BindEnv(key, env); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // LoadConfigs reads the env variables into a Config struct.
 func LoadConfigs() (*Config, error) {
-	viper.SetConfigFile(".env")
-	_ = viper.ReadInConfig() // ignore if .env missing
+	v := viper.New()
 
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
 
-	viper.BindEnv("FLAP_LOG_LEVEL")
-	viper.BindEnv("FLAP_JSON_LOG")
-	viper.BindEnv("FLAP_TLS")
+	v.SetConfigFile(".env")
+	_ = v.ReadInConfig()
 
 	var cfg Config
-	if err := viper.Unmarshal(&cfg); err != nil {
+
+	if err := bindEnvs(v, &cfg); err != nil {
+		return nil, err
+	}
+
+	if err := v.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
 
