6a0b212172766b7604b58cba3fedf8f5f0c03c1e
amirhnajafiz <najafizadeh21@gmail.com>
2025-12-24 12:05:03 -0500

    update: manifests

--- operator/include/configs/default.go
+++ operator/include/configs/default.go
@@ -9,6 +9,8 @@ func Default() *viper.Viper {
 	v.SetDefault("log_level", "info")
 	v.SetDefault("json_log", false)
 	v.SetDefault("tls.enable", false)
+	v.SetDefault("tls.cert_path", "/etc/flap/tls/tls.crt")
+	v.SetDefault("tls.key_path", "/etc/flap/tls/tls.key")
 
 	return v
 }
